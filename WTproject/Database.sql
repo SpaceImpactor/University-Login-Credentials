03a12e6cb4da8f8a76058409eb2029b58f309dc6
Avi <avimnk@gmail.com>
2017-11-09 13:12:29 +0530

    Added Database File
    Database for Login and Staff.
    
    Created in MySql Workbench.

---
+++ WTproject/Database.sql
@@ -0,0 +1,25 @@
+create database login;
+use login;
+create table logged
+(
+usr char(10),
+pwd varchar(10),
+role char(10)
+);
+insert into logged values('1','abc','student');
+insert into logged values('2','def','staff');
+insert into logged values('3','ghi','admin');
+
+select * from log ged;
+
+create table staff
+(
+class char(10),
+yr int,
+peroid int,
+attendance char(20)
+);
+
+select * from staff;
+
+
